630e8bfaef208d729a09a3904c7129ea86770b14
Harshit Raghuvanshi <rharshit98@gmail.com>
2019-03-10 13:39:21 +0530

    UI: Add basic layout

--- app/src/main/java/com/rharshit/flickrsearch/MainActivity.java
+++ app/src/main/java/com/rharshit/flickrsearch/MainActivity.java
@@ -2,12 +2,23 @@ package com.rharshit.flickrsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etSeatch;
+    Button bSearch;
+    GridView gvImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etSeatch = findViewById(R.id.et_search);
+        bSearch = findViewById(R.id.b_search);
+        gvImages = findViewById(R.id.gv_images);
     }
 }
